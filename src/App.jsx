b0ab15a5d369081825a87fc1c8a86258add881e5
YuvalOfek1 <yuvalofek97@gmail.com>
2023-12-31 12:01:50 +0200

    commit frontend

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import ResponsiveAppBar from './components/NavBar/NavBar'
+import { HomePage } from './pages/HomePage/HomePage'
+import { Login } from './pages/Login/Login'
+import { NoPage } from './pages/NoPage/NoPage'
+import { Lobby } from './pages/Lobby/Lobby'
+
+function App() {
+ 
+  return (<>
+    <BrowserRouter>
+    <ResponsiveAppBar/>
+      <Routes>
+          <Route path="/" element={<ResponsiveAppBar/>}/>
+          <Route path="/" element={<HomePage />}/>
+          <Route index element={<HomePage />} />
+          <Route path="lobby" element={<Lobby />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+    </>
+    
+    
+    
+  )
+}
+
+export default App
