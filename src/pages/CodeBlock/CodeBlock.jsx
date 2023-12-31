7f59f8f8aec235a6ee360197a788eb79b73bd62f
YuvalOfek1 <yuvalofek97@gmail.com>
2023-12-31 22:56:45 +0200

    socket io changed

--- src/pages/CodeBlock/CodeBlock.jsx
+++ src/pages/CodeBlock/CodeBlock.jsx
@@ -44,7 +44,7 @@ export const CodeBlock = () => {
     console.log("count from serverSocket", count);
   };
   useEffect(() => {
-    const newSocket = io('https://moveoback.onrender.com', { transports: ['websocket']});
+    const newSocket = io('wss://moveoback.onrender.com/');
     
     setSocket(newSocket);
     newSocket.on("count", (count) => {
